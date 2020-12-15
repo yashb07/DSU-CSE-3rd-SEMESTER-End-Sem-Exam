c84211697ba1f5258c5a487f0c5389a50246ca81
Swagnikk Bhattacharya <59500145+swagnikk@users.noreply.github.com>
2020-12-15 21:22:10 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,12 @@
+s1=input("Enter a string: ")
+s2=input("Enter second string: ")
+if(sorted(s1)==sorted(s2)):
+    print("Strings are anagrams.")
+else:
+    print("Oops! Not anagrams.")
+"""
+Created on Sat Jul 18 13:32:43 2020
+
+@author: swagn
+"""
+
